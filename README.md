7c36a34cc930bd4345a030e676d4d9aa8ae16f95
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-10-04 00:51:36 +0530

    I'm Back

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # IEDC-SJCET
-IEDC BOOTCAMP, SJCET PALAI unofficial website.
 
-Lot of work to do. It just a sample. 
-Now Let me take some time to study for my Semester Exam
-
-[Here](https://rajatsandeepsen.github.io/IEDC/iedc.html)
+im Back
---
+++ script.js
@@ -0,0 +1,11 @@
+// var ss = document.getElementsByClassName("ss");
+  
+
+function show_secondary(){
+    var x = document.getElementById("showbutton");
+    x.style.display = "none";
+    setTimeout(
+    "window.scrollBy(0, 670)"
+        , 200);
+
+}
