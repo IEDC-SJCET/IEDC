cfc7f7ddfcc28d89fa9b25229ced79269821e93c
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-10-07 01:14:39 +0530

    busy

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// var ss = document.getElementsByClassName("ss");
-  
 
-function show_secondary(){
-    var x = document.getElementById("showbutton");
-    x.style.display = "none";
-    setTimeout(
-    "window.scrollBy(0, 670)"
-        , 200);
-
-}
-
-// var y = document.getElementById("changetext");
-// var text = ["Innovative","&nbsp; Creative &nbsp;","&nbsp;Techincal&nbsp;"];
-// let i=0;
-// setInterval(function(){
-//     i = (i+1)%3;
-//     y.innerHTML = text[i];
-// },2000)
