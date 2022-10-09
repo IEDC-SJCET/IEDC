55814f02bd6279b2822657019e9974409e7244f3
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-10-09 17:49:22 +0530

    qwert

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 var extraTabs = document.getElementById('extraTabs');
+var openButton = document.getElementById('openButton');
+
 function openAll(){
-  extraTabs.style.display = 'flex';
+  if (extraTabs.style.display == 'none'){
+    extraTabs.style.display = 'flex';
+    openButton.childNodes[1].classList.remove("bi-chevron-down");
+    openButton.childNodes[1].classList.add("bi-chevron-up");
+  }
+  else{
+  extraTabs.style.display = 'none';
+  openButton.childNodes[1].classList.remove("bi-chevron-up");
+  openButton.childNodes[1].classList.add("bi-chevron-down");
+  }
 }
