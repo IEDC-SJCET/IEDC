3a0c7b7ed8dac4956ae9973cd8a6d7b8ea8869a9
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2022-10-18 12:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ function openEventDetails(y){
   var x = document.getElementById(y);
   x.style.display = "flex";
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
