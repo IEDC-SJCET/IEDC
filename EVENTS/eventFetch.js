50acd4abafff1e2dcd4f5018726c62ba11148eff
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-12-26 23:19:42 +0530

    [feature] done external event fetching

--- EVENTS/eventFetch.js
+++ EVENTS/eventFetch.js
@@ -1,9 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
 import { getFirestore, collection, getDocs, query, orderBy } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore-lite.js';
 
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
                 apiKey: "AIzaSyCNbmkHVo6YAOk69h9OgMGbQJBUlW5xz4c",
                 authDomain: "iedc-admin.firebaseapp.com",
@@ -15,38 +12,52 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 const DB  = getFirestore();
 
+let collider = (window.innerWidth < 768)? "collapse img-fluid" : "collapse-horizontal collapse img-fluid";
 let valid = {
-        btn : "btn-success",
+        btnBG : "",
+        btnCOLOR: "greencolor",
         btnText : "Reg Now",
         icon: "bi bi-chevron-right"
     }
 let invalid = {
-        btn : "btn-light",
+        btnBG : "",
+        btnCOLOR: "text-black-50",
         btnText : "Expired",
         icon: "bi bi-clock"
     }
 
 const templete = (data) => {
-    console.log(validity);
-    return `<div class="row col  border-0 justify-content-center m-0">
-                <div class="col-12 col-md-6 bg-transparent  card p-0 border-0 m-0">
-                    <img src="${ s}" alt="" srcset="" class="img-fluid ">
-                    <button class="card-img-overlay h-100 w-100" onclick="openEventDetails(${ a})"></button>
+    let validity = data.LinkExpireAt > Date.now()? valid : invalid;
+    data.Description = data.Description.toString().replace(/(?:\r\n|\r|\n)/g, '<br>');
+    let collapseTarget = data.EventName.toString().replace(/ +/g,"");
+    return `
+            <div class="d-flex flex-column flex-md-row justify-content-center gap-3">
+                <div style="max-width: 400px; max-height: 400px;">
+                    <a data-bs-toggle="collapse" aria-expanded="true" data-bs-target="#${collapseTarget}" aria-controls="${collapseTarget}">
+                        <img src=${data.IMG_URL} alt="EventIMG" class="img-fluid rounded-3">
+                    </a>
                 </div>
-                <div style="display: none;" id=${c}
-                    class="card rounded-3 shadow-lg  col-12 col-md-5  bg-light eventbody ms-md-3 mt-3 mt-md-0 p-3 text-black flex-column justify-content-center">
-                    <h2 class="fs-4 fw-bold text-black">${d}</h2>
-                    <h5 class="fs-7 text-black-50">${e}</h5><br>
-                    <h5 class="fs-7 text-black-50">${f}</h5><br>
-                    <span class="mx-auto">
-                        <a href=${g} class="btn btn-outline-dark rounded-5">${h}<i class=${i}></i></a>
-                    </span>
+                <div class="${collider}" id="${collapseTarget}"
+                    style="max-width: 400px; max-height: 400px;">
+                    <div class="card shadow-lg  text-black">
+                        <div class="fs-4 fw-bold text-black card-header">${data.EventName}</div>
+                        <div class="fs-7 text-black-50 card-body">
+                            <p>${data.Description}</p>
+                            <p>${null}</p>
+                        </div>
+                        <div class="card-footer ${validity.btnBG} text-end">
+                        <a href=${data.RedirectLink} class="${validity.btnCOLOR} py-2 fs-6 fw-bold">
+                            ${validity.btnText} <i class="${validity.icon}"></i>
+                        </a>
+                        </div>
+                    </div>
                 </div>
-            </div>`;
+            </div>
+            `;
 }
 
 const EVENTS = collection(DB,'EVENTS');
-const qry = query(EVENTS, orderBy('timeStamp'));
+const qry = query(EVENTS, orderBy('EventStartsAt', "desc"));
 const eventContainer = document.getElementById("eventSECTION");
     eventContainer.innerHTML = "";
     const querySnapshot = await getDocs(qry);
@@ -54,6 +65,3 @@ const eventContainer = document.getElementById("eventSECTION");
         let data = doc.data();
         eventContainer.innerHTML += templete(data);
     });
-
-
-// str = str.replace(/(?:\r\n|\r|\n)/g, '<br>');
