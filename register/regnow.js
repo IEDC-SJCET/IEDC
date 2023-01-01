51ab775c9c298b2f2ef78f9a52a8a5e3f9e16dfe
RajatSandeep <rajatsandeepsen1839@gmail.com>
2023-01-01 18:27:56 +0530

    [new] common registration form

---
+++ register/regnow.js
@@ -0,0 +1,36 @@
+import {openSpinner, submitDone, submitNOTDone} from '/src/script/main.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
+import { addDoc, getFirestore, collection } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore-lite.js';
+const firebaseConfig = {
+                apiKey: "AIzaSyCNbmkHVo6YAOk69h9OgMGbQJBUlW5xz4c",
+                authDomain: "iedc-admin.firebaseapp.com",
+                projectId: "iedc-admin",
+                storageBucket: "iedc-admin.appspot.com",
+                messagingSenderId: "200933316108",
+                appId: "1:200933316108:web:8b5d08b6295d0962ec8029"
+            };
+initializeApp(firebaseConfig);
+const DB  = getFirestore();
+const FORM = collection(DB,'FORMS');
+
+
+
+const SUBMITFORM = document.getElementById('SUBMITFORM');
+SUBMITFORM.addEventListener('submit',e => {
+        e.preventDefault();
+        openSpinner();
+        addDoc(FORM, {
+            // here is the actual code lives
+                    Name: SUBMITFORM.Name.value,
+                    Email: SUBMITFORM.Email.value,
+                    Phone: SUBMITFORM.Phone.value,
+                    UploadTimeStamp: Date.now()
+                }).then(() => {
+                    SUBMITFORM.reset();
+                    submitDone();
+                })
+                .catch((error) => {
+                    console.error("Error adding document: ", error);
+                    submitNOTDone();
+                });
+})
